41d8bfea550a383d972e726d79f508c1c4dd5114
PeytonSpreacker <peyton@spreacker.com>
2022-04-05 19:52:18 -0500

    initial commit and stalemate condition

---
+++ rockpaperscissors.py
@@ -0,0 +1,8 @@
+# player 1 inputs their selection
+player1 = input('Player 1: rock, paper, or scissors? ')
+
+# player 2 inputs their selection
+player2 = input('Player 2: rock, paper, or scissors? ')
+
+if player1 == player2:
+    print('Stalemate')
