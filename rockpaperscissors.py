5c6506671d07b7052f785ff4b0a8fe3cd9f871ff
PeytonSpreacker <peyton@spreacker.com>
2022-04-06 09:01:02 -0500

    decreased sleep timer

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -25,34 +25,34 @@ def game():
     # game conditions
     if player1 == player2:
         print("Stalemate")
-        time.sleep(2.0)
+        time.sleep(1.0)
     elif player1 == "rock":
         if player2 == "scissors":
             print("Rock smashes scissors! Player 1 wins!")
             score_p1 = score_p1 + 1
-            time.sleep(2.0)
+            time.sleep(1.0)
         else:
             print("Paper covers rock! Player 2 wins")
             score_p2 = score_p2 + 1
-            time.sleep(2.0)
+            time.sleep(1.0)
     elif player1 == "paper":
         if player2 == "rock":
             print("Paper covers rock! Player 1 wins!")
             score_p1 = score_p1 + 1
-            time.sleep(2.0)
+            time.sleep(1.0)
         else:
             print("Scissors cuts paper! Player 2 wins.")
             score_p2 = score_p2 + 1
-            time.sleep(2.0)
+            time.sleep(1.0)
     elif player1 == "scissors":
         if player2 == "paper":
             print("Scissors cuts paper! Player 1 wins!")
             score_p1 = score_p1 + 1
-            time.sleep(2.0)
+            time.sleep(1.0)
         else:
             print("Rock smashes scissors! Player 2 wins.")
             score_p2 = score_p2 + 1
-            time.sleep(2.0)
+            time.sleep(1.0)
     else:
         print("Invald input! Please try again.")
     print("Player 1: ", score_p1)
